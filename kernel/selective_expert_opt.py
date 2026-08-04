c3ffc74fe531a47b5eb09063deba6e08e58b2278
zigeng <abc1597176668@outlook.com>
2026-08-04 03:44:26 +0000

    fuse gate-up cross-core exchange

--- kernel/selective_expert_opt.py
+++ kernel/selective_expert_opt.py
@@ -33,7 +33,7 @@ from .mlp_parameters import (
 # MLP utils
 from .mlp_tkg_constants import MLPTKGConstants
 from .mlp_tkg_down_projection import process_down_projection
-from .mlp_tkg_gate_up_projection import process_gate_up_projection
+from .mlp_tkg_gate_up_projection_dispatch_opt import process_gate_up_projection
 from .moe_tkg_utils import (
     broadcast_token_affinity,
     gather_expert_affinities,
@@ -462,5 +462,4 @@ def _select_quant_scales(quant_params: MLPQuantizationParameters, expert_id_offs
         down_in_scale=down_in_scale_view,
         clipping_bound=quant_params.clipping_bound,
     )
-
 
